3cac3b3ea7574e576fe2ee9e2378a78afbc1744b
HakanK003 <hkaratashk3@gmail.com>
2022-04-29 11:56:37 -0400

    Hacker Rank practice 7

---
+++ src/fromHackerRank/practice7/JavaInheritanceI.java
@@ -0,0 +1,38 @@
+package fromHackerRank.practice7;
+
+public class JavaInheritanceI {
+}
+
+//java 8
+
+class Animal{
+    void walk()
+    {
+        System.out.println("I am walking");
+    }
+}
+class Bird extends Animal
+{
+    void fly()
+    {
+        System.out.println("I am flying");
+    }
+
+    void sing()
+    {
+        System.out.println("I am singing");
+    }
+
+}
+
+class Solution{
+
+    public static void main(String args[]){
+
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+
+    }
+}
---
+++ src/fromHackerRank/practice7/JavaSingletonPattern.java
@@ -0,0 +1,30 @@
+package fromHackerRank.practice7;
+
+public class JavaSingletonPattern {
+}
+
+//java 8
+
+class Singleton{
+
+    private static Singleton singleton;
+
+    String str;
+
+    private Singleton(){}
+
+    public static Singleton getSingleInstance(){
+
+        if (singleton==null)
+        {
+            return new Singleton();
+        }
+        else
+        {
+            return singleton;
+        }
+
+    }
+
+
+}
