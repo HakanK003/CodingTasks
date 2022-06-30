097218b37eb1d65fad851c7dae3e121a93d8ad73
HakanK003 <hkaratashk3@gmail.com>
2022-06-29 22:46:23 -0400

    PrimeNumbers.java

---
+++ src/commonInterviewTasks/tasksFromYK/PrimeNumbers.java
@@ -0,0 +1,22 @@
+package commonInterviewTasks.tasksFromYK;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(7));
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        } else {
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
