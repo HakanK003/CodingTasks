b970f4a2a1c828b9122614b00381f4d920833884
HakanK003 <hkaratashk3@gmail.com>
2022-06-30 23:05:04 -0400

    FindUniquesWithMap.java

---
+++ src/commonInterviewTasks/tasksFromYK/FindUniquesWithMap.java
@@ -0,0 +1,34 @@
+package commonInterviewTasks.tasksFromYK;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindUniquesWithMap {
+
+    public static void main(String[] args) {
+        System.out.println(uniques("aaabcccdef"));
+    }
+
+    private static List<Character> uniques(String str) {
+        List<Character> result = new ArrayList<>();
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        for (char c : str.toCharArray()) {
+            if (!map.containsKey(c)) {
+                map.put(c, 1);
+            } else {
+                map.put(c, map.get(c) + 1);
+            }
+        }
+
+        map.forEach((k, v) -> {
+            if (v == 1) {
+                result.add(k);
+            }
+        });
+        return result;
+    }
+
+}
