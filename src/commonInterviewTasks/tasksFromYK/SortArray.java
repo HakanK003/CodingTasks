5ed3b79962297093c46f9c82361c3ea1c52e0a66
HakanK003 <hkaratashk3@gmail.com>
2022-07-07 21:05:38 -0400

    SortArray.java

---
+++ src/commonInterviewTasks/tasksFromYK/SortArray.java
@@ -0,0 +1,26 @@
+package commonInterviewTasks.tasksFromYK;
+
+import java.util.Arrays;
+
+public class SortArray {
+
+    public static void main(String[] args) {
+        int[] nums={4,1,6,2,8,5,9,3,7};
+        System.out.println(Arrays.toString(sortArray(nums)));
+    }
+
+    private static int[] sortArray(int[] nums) {
+        int temp;
+        for (int i = 0; i < nums.length-1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if(nums[j]<nums[i]){
+                    temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+        return nums;
+    }
+
+}
