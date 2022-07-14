aafcd040ab258dca90c5bc40c96296bd7bac0d17
HakanK003 <hkaratashk3@gmail.com>
2022-07-13 23:45:48 -0400

    RemoveDuplicates.java

---
+++ src/commonInterviewTasks/tasksFromYK/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+package commonInterviewTasks.tasksFromYK;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        System.out.println(remove("google"));
+    }
+
+    private static String remove(String str) {
+        String removed="";
+        for(int i=0; i<str.length(); i++){
+            if(!removed.contains(""+str.charAt(i))){
+                removed+=str.charAt(i);
+            }
+        }
+        return removed;
+    }
+
+}
