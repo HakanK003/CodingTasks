1e6baa59b1757dd655279a63799b2789475580d5
HakanK003 <hkaratashk3@gmail.com>
2022-07-19 23:53:51 -0400

    RemoveSameFirstLastChar.java

---
+++ src/commonInterviewTasks/tasksFromYK/RemoveSameFirstLastChar.java
@@ -0,0 +1,17 @@
+package commonInterviewTasks.tasksFromYK;
+
+public class RemoveSameFirstLastChar {
+
+    public static void main(String[] args) {
+        String str = "XwordX";
+        System.out.println(removeFirstLast(str));
+    }
+
+    private static String removeFirstLast(String str) {
+        if(str.charAt(0)==str.charAt(str.length()-1)){
+            str=str.substring(1,str.length()-1);
+        }
+        return str;
+    }
+
+}
