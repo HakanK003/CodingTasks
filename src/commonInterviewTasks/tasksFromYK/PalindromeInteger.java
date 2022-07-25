be721b540af805cbd2d50b53a71cd65b7ed215d0
HakanK003 <hkaratashk3@gmail.com>
2022-07-24 23:02:17 -0400

    PalindromeInteger.java

---
+++ src/commonInterviewTasks/tasksFromYK/PalindromeInteger.java
@@ -0,0 +1,25 @@
+package commonInterviewTasks.tasksFromYK;
+
+public class PalindromeInteger {
+
+    public static void main(String[] args) {
+        // https://leetcode.com/problems/palindrome-number/
+        System.out.println("palindrome = " + isPalindrome(121));
+    }
+
+    public static boolean isPalindrome(int num){
+        if (num<0) {
+            return false;
+        }
+        int temp=num;
+        int reverse=0;
+
+        while (temp>0){
+            reverse*=10;
+            reverse+=temp%10;
+            temp/=10;
+        }
+        return reverse == num;
+    }
+
+}
