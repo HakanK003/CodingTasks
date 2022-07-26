b4616d05a32044040aa6b4cd71038ad209cf171e
HakanK003 <hkaratashk3@gmail.com>
2022-07-25 23:22:38 -0400

    FindUniquesWithNestedLoop.java

---
+++ src/commonInterviewTasks/tasksFromYK/FindUniquesWithNestedLoop.java
@@ -0,0 +1,26 @@
+package commonInterviewTasks.tasksFromYK;
+
+public class FindUniquesWithNestedLoop {
+
+    public static void main(String[] args) {
+        String str = "google";
+        System.out.println(findUniques(str));
+    }
+
+    static String findUniques(String str){
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            int count=0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i)==str.charAt(j)){
+                    count++;
+                }
+            }
+            if (count == 1) {
+                result += str.charAt(i);
+            }
+        }
+        return result;
+    }
+
+}
