704ccbac17e2dbd8d38582349101222558dd16c0
HakanK003 <hkaratashk3@gmail.com>
2022-07-26 22:26:50 -0400

    AbstractClassTest.java

---
+++ src/randomTasks/AbstractClassTest.java
@@ -0,0 +1,36 @@
+package randomTasks;
+
+public class AbstractClassTest {
+
+    static class helloThere{
+
+    }
+
+}
+
+abstract class aaa111{
+
+    public int a1 = 5;
+    int a2 =5;
+    protected int a3 = 5;
+    private int a4 = 5;
+
+
+      public static void hello(){
+        System.out.println("asdfsd");
+    }
+
+}
+
+interface ccc{
+
+    int aaa = 1;
+
+
+}
+
+class bbb222 extends aaa111{
+
+
+
+}
