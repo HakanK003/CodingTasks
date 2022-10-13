c90ae9ffb75c980f2981d7bbbe9da63abd712317
HakanK003 <hkaratashk3@gmail.com>
2022-10-13 09:22:49 -0400

    DeleteNodeInALinkedList_237.java

---
+++ src/fromLeetCode/medium/DeleteNodeInALinkedList_237.java
@@ -0,0 +1,31 @@
+package fromLeetCode.medium;
+
+public class DeleteNodeInALinkedList_237 {
+
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Delete Node in a Linked List.
+    //Memory Usage: 41.8 MB, less than 89.82% of Java online submissions for Delete Node in a Linked List.
+    public void deleteNode(ListNode node) {
+
+        //just
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+
+//        if(node==null || node.next==null) return;
+//
+//        node.val = node.next.val;
+//        node.next= node.next.next;
+//        deleteNode(node.next);
+
+    }
+
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
