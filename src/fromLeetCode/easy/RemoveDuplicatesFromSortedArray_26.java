cc294245b6d047ece348c52805eae34c1c956cd8
HakanK003 <hkaratashk3@gmail.com>
2022-10-26 09:23:34 -0400

    RemoveDuplicatesFromSortedArray_26.java

---
+++ src/fromLeetCode/easy/RemoveDuplicatesFromSortedArray_26.java
@@ -0,0 +1,29 @@
+package fromLeetCode.easy;
+
+/*
+    Related Topics
+    -> Array
+    -> Two Pointers
+ */
+
+public class RemoveDuplicatesFromSortedArray_26 {
+
+    //Runtime: 2 ms, faster than 68.03% of Java online submissions for Remove Duplicates from Sorted Array.
+    //Memory Usage: 47.7 MB, less than 62.68% of Java online submissions for Remove Duplicates from Sorted Array.
+    public int removeDuplicates(int[] nums) {
+
+        int different =1;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i]!=nums[i-1]) {
+                nums[different] = nums[i];
+                different++;
+            }
+
+        }
+
+        return different;
+    }
+
+}
