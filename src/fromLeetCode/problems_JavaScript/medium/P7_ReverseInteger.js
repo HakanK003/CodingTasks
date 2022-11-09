e02dfe8b2cc0d1b9d1c2079aead17a4976fa09af
HakanK003 <hkaratashk3@gmail.com>
2022-11-08 22:07:52 -0500

    P7_ReverseInteger.js

---
+++ src/fromLeetCode/problems_JavaScript/medium/P7_ReverseInteger.js
@@ -0,0 +1,32 @@
+//Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+ /*
+     Related Topics
+     -> Math
+ */
+
+// S1 - Math
+// Time Complexity O(log(n)) | Space Complexity O(1)
+var reverse = function(x) {
+
+        var rev = 0;
+
+        while (x != 0) {
+
+            var pop = x % 10;
+            x = Math.trunc(x / 10);
+
+            if ( (rev > 214748364) || ((rev == 214748364) && (pop > 7)) ) { return 0;}
+            if ( (rev < -214748364) || ((rev == -214748364) && (pop < -8)) ) { return 0;}
+
+            rev = (rev * 10) + pop;
+
+        }
+
+        return rev;
+
+};
