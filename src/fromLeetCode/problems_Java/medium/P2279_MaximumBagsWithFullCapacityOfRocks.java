048adfba65d7f80abbcee42f219db58df9408f99
HakanK003 <hkaratashk3@gmail.com>
2022-12-27 23:41:41 -0500

    P2279_MaximumBagsWithFullCapacityOfRocks.java

---
+++ src/fromLeetCode/problems_Java/medium/P2279_MaximumBagsWithFullCapacityOfRocks.java
@@ -0,0 +1,78 @@
+package fromLeetCode.problems_Java.medium;
+
+import java.util.Arrays;
+
+/*
+    Related Topics
+    -> Array
+    -> Greedy
+    -> Sorting
+
+    Problem
+    -> https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/
+
+        You have n bags numbered from 0 to n - 1. You are given two 0-indexed integer arrays capacity and rocks.
+        The ith bag can hold a maximum of capacity[i] rocks and currently contains rocks[i] rocks.
+        You are also given an integer additionalRocks, the number of additional rocks you can place in any of the bags.
+        Return the maximum number of bags that could have full capacity after placing the additional rocks in some bags.
+
+    Examples
+    -> Example 1
+            Input: capacity = [2,3,4,5], rocks = [1,2,4,4], additionalRocks = 2
+            Output: 3
+            Explanation:
+                Place 1 rock in bag 0 and 1 rock in bag 1.
+                The number of rocks in each bag are now [2,3,4,4].
+                Bags 0, 1, and 2 have full capacity.
+                There are 3 bags at full capacity, so we return 3.
+                It can be shown that it is not possible to have more than 3 bags at full capacity.
+                Note that there may be other ways of placing the rocks that result in an answer of 3.
+    -> Example 2
+            Input: capacity = [10,2,2], rocks = [2,2,0], additionalRocks = 100
+            Output: 3
+            Explanation:
+                Place 8 rocks in bag 0 and 2 rocks in bag 2.
+                The number of rocks in each bag are now [10,2,2].
+                Bags 0, 1, and 2 have full capacity.
+                There are 3 bags at full capacity, so we return 3.
+                It can be shown that it is not possible to have more than 3 bags at full capacity.
+                Note that we did not use all of the additional rocks.
+
+    Constraints
+    -> n == capacity.length == rocks.length
+    -> 1 <= n <= 5 * 10^4
+    -> 1 <= capacity[i] <= 10^9
+    -> 0 <= rocks[i] <= capacity[i]
+    -> 1 <= additionalRocks <= 10^9
+ */
+
+
+
+/**
+ * Approach 1 -> Greedy
+ *
+ *
+ *
+ * Time Complexity ->
+ *
+ *
+ * Space Complexity ->
+ *
+ * Result
+ *   Runtime 15 ms Beats 94.28% | Memory 53.6 MB Beats 78.1%
+ *
+ * Reference
+ *   https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/solutions/2061949/greedy-intuition-o-1-space-c-java/?orderBy=most_votes
+ */
+class Solution2279_1_Greedy {
+    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        int n = rocks.length, cnt=0;
+        for(int i = 0;i < n; i++) rocks[i] = capacity[i] - rocks[i];
+        Arrays.sort(rocks);
+        for(int i = 0;i < n && (rocks[i] - additionalRocks) <= 0;i++) {
+            cnt++;
+            additionalRocks -= rocks[i];
+        }
+        return cnt;
+    }
+}
